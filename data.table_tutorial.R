8dfea95ace20100236dba4bbad62621e14670490
azizemabg <azizemabg@gmail.com>
2023-07-06 20:58:40 +0200

    Last commit of tutorial for using data.table

--- data.table_tutorial.R
+++ data.table_tutorial.R
@@ -185,5 +185,191 @@ setorder(flight, month, day, -carrier)
 # we can do any operation on rows by adding := operator
 # In this example we are substracting dep_delay variable from dep_time variable
 # to compute scheduled departure time
+
+# Adding or creating new single column based on calculating existing column
 flight[, dep_sch := dep_time - dep_delay]
 View(flight)
+
+# Adding multiple columns 
+flight[, c("dep_sch", "arr_sch") := 
+         .(dep_time - dep_delay, arr_time - arr_delay)] # .() is same as list()
+flight[, c("dep_sch", "arr_sch") := 
+         list(dep_time - dep_delay, arr_time - arr_delay)] # same as above
+
+# If then Else ####
+# there are two different method
+# method 1: flight[, flag:= 1*(min < 50)]
+# method 2: flight[, flag:= ifelse(min < 50, 1, 0)]
+flight[, delay_status :=
+         ifelse(dep_delay > 60, "long delay", ifelse(dep_delay == 30, "medium delay", "short delay"))]
+flight[delay_status == "long delay"][order(dep_delay)]
+flight[delay_status == "short delay"][order(dep_delay)]
+
+# Writing sub Queries like SQL
+# format DT[][][]
+flight[, dep_sch :=
+         dep_time - dep_delay][, .(dep_time, dep_delay, dep_sch)]
+# we are computing scheduled departure time and then selecting only
+# relevant columns
+
+# Summarize and aggregate columns ####
+flight[, .(mean = mean(arr_delay, na.rm = TRUE), 
+           min = min(arr_delay, na.rm = TRUE), 
+           max = max(arr_delay, na.rm = TRUE))]
+
+# summarize multiple columns
+# to summarize multiple variables, we can simply write all the summary
+# statistics function in bracket. see below:
+flight[, .(mean(dep_time), mean(dep_delay))]
+
+# If you want to calculate summary statistics for a larger list of variables, 
+# you can use .SD and .SDcols operators. 
+# The .SD operator implies "subset of data"
+flight[, lapply(.SD, mean), .SDcols = c("arr_delay", "dep_delay")]
+
+# Summarize all numeric columns 
+flight[, lapply(.SD, mean)]
+
+# summarize wih multiple statistics
+flight[, sapply(.SD, function(x) c(mean = mean(x), median = median(x), 
+                                   var = var(x), sd = sd(x), 
+                                   range = range(x)))]
+
+# using group by (within group calculation) ####
+flight[, .(mean_arr_delay = mean(arr_delay, na.rm = TRUE)), 
+       by = origin]
+
+# grouped by two variables
+flight[, .(mean_arr_delay = mean(arr_delay, na.rm = TRUE)), 
+       by = .(origin, carrier)][order(-mean_arr_delay)]
+
+# Use key column in a by operation 
+# instead of by, you can use keyby = operator
+flight[, .(mean_arr_delay = mean(arr_delay, na.rm = TRUE)), 
+       keyby = origin]
+
+# summarize multiple variables by group "origin"
+flight[, .(mean_arr_delay = mean(arr_delay, na.rm = TRUE), 
+           mean_dep_delay = mean(dep_delay, na.rm = TRUE)), 
+       by = origin]
+flight[, lapply(.SD, mean, na.rm = TRUE), 
+       .SDcols = c("arr_delay", "dep_delay"), 
+       by = origin] # same as above code
+
+# Remove duplicates ####
+# you can remove non-unique / duplicates cases with unique()
+# For example, you want to eliminate duplicates based on variable carrier
+setkey(flight, "carrier")
+unique(flight)
+
+# Remove duplicated based on all the variables
+setkey(flight, NULL)
+key(flight)
+unique(flight)
+
+# Extract values within group
+# Select the first and second values from categorical variable carrier
+flight[, .SD[1:2, 
+             by = carrier]] 
+flight[, .SD[1:2, 
+             by = carrier]] == flight[1:2, ]
+
+# Select last value from a group
+flight[, .SD[.N], 
+       by = carrier]
+
+# Applyig frank function 
+flight.rank <- flight[, rank:= frank(-distance, ties.method = "min"), 
+                      by = carrier][sort(rank)]
+unique(flight.rank$rank)
+View(flight.rank)
+?frank
+
+# Cummulative sum by group ####
+flight[, cum :=
+         cumsum(distance), 
+       by = carrier]
+
+# Between and like operator
+dt <- data.table(x = 6:10)
+dt
+dt[x %between% c(7,9)] # selecting real value in that variable
+
+# %like% is mainly used to find all the values that matches a pattern
+dt <- data.table(name = c("dep_time", "dep_delay", "dep_sch", "dep_total", 
+                          "arr_sch", "flight", "destination"), id = c(1:7))
+dt
+dt[name %like% "dep"]
+
+# merging and join ####
+# merging in data.table is similar to base r function called merge()
+# The diffence is by default takes common key variables as a primary key to merge
+# two datasets. data.frame takes common variable names a primary key to merge a dataset
+
+dt1 <- data.table(A = letters[rep(1:3, 2)], X = 1:6, 
+                  key = "A")
+dt1
+dt2 <- data.table(A = letters[rep(2:4, 2)], Y = 6:1, 
+                  key = "A")
+dt2
+
+# inner join
+# It returns all the matching observations in both the datasets
+merge(dt1, dt2, by = "A")
+
+# Left join
+# it returns all observations from the left dataset and the matched
+# observations from the right dataset
+merge(dt1, dt2, by = "A", all.x = TRUE)
+
+# rigth join
+merge(dt1, dt2, by = "A", all.y = TRUE)
+
+# Full join
+# It return all rows when there is a match in one of the datasets
+merge(dt1, dt2, all = TRUE)
+
+# Convert data.table to data.fram
+# using setDF() function
+
+setDF(mydata)
+set.seed(111)
+x <- data.frame(a = sample(3, 10, TRUE), 
+                b = sample(letters[1:3], 10, TRUE))
+x
+class(x)
+setDT(x, key = "A")
+setDT(x, key = "a")
+
+# Reshaping data using data.table
+# for reshaping or transposing data, we can use dcast.data.table()
+# and melt.data.table() function
+# it is from reshape2 package
+
+# Calculate the total number of rows by month and then sort on descending order
+flight[, .N, by = month][order(-N)]
+# .N ==> calculate the number of rows or records for particular column
+# indicated by = function
+# .N operator is used to find count
+
+# Find top 3 months with high mean arrival delay
+flight[, .(mean_arr_delay = mean(arr_delay, na.rm = TRUE)),
+       by = month][order(-mean_arr_delay)][1:3]
+
+# Find the origin of flights having average total delay is greater than 20 min
+flight[, lapply(.SD, mean, na.rm = TRUE), 
+       .SDcols = c("arr_delay", "dep_delay"), 
+       by = origin][(arr_delay + dep_delay) > 20]
+
+# Extract average of arrival and departure delays for carrier == "DL"
+# by origin and destination variables
+flight[carrier == "DL", 
+       lapply(.SD, mean, na.rm = TRUE), #calculating for mean for columns
+       by = .(origin, Destination), 
+       .SDcols = c("arr_delay", "dep_delay")] #the columns where indicated by .SD, mean
+
+# Pull first value of "air_time" by "origin" and then sum the returned values
+# when it is greater than 300
+
+flight[, .SD[1], .SDcols = "air_time", 
+       by = origin][air_time > 300, sum(air_time)]
